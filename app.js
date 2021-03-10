603654de11601e0cb17c4b3382b1532818212e76
Sergii <sergiikovtun.vn@gmail.com>
2021-03-10 12:01:17 +0000

    test vue

--- app.js
+++ app.js
@@ -4,7 +4,11 @@ const app = Vue.createApp({
             myTitle: 'Старт Vue',
             vueLinck: 'https://vuejs.org/',
             goalA: 'Закінчити курс і Вивчити Вю',
-            goalB: '<h3>Закінчити курс і написати сайт на Вю</h3>'
+            goalB: '<h3>Закінчити курс і написати сайт на Вю</h3>',
+            yourName: 'Sergii',
+            yourAge: 30,
+            linkPhoto: 'https://photo-works.net/images/effects-2/effect-2-5.jpg',
+            inputValue: ''
         };
     },
     methods: {
@@ -24,7 +28,6 @@ const app = Vue.createApp({
                 return this.goalB
             }
         }
-
     }  
 });
 
